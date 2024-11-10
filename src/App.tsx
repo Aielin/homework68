851f7a6aa09e9ddef7564fa5d0b7a8f68543eae3
Aielina <aielina.madambekova@gmail.com>
2024-11-10 18:51:49 +0600

    Added Bootstrap styling to TodoList component for improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import TodoList from './containers/Todo/todoList.tsx';
 
 const App = () => {
   return (
-    <div className="container mt-5">
+    <div>
       <TodoList/>
     </div>
   );
--- src/containers/Todo/todoList.tsx
+++ src/containers/Todo/todoList.tsx
@@ -6,13 +6,13 @@ const TodoList: React.FC = () => {
   const todos = useSelector((state: RootState) => state.todos.items);
 
   return (
-    <div>
-      <h2>Todo List</h2>
-      <ul>
+    <div className="container mt-4">
+      <h2 className="text-center">Todo List</h2>
+      <ul className="list-group">
         {todos.map((todo) => (
-          <li key={todo.id}>
-            <input type="checkbox" checked={todo.completed} readOnly />
-            {todo.title}
+          <li key={todo.id} className="list-group-item d-flex align-items-center">
+            <input type="checkbox" checked={todo.completed} readOnly className="me-3"/>
+            <span>{todo.title}</span>
           </li>
         ))}
       </ul>
